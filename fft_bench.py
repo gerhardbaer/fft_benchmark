dfcf04e854cfd5e33ed38fc0e3ef9b4b89144058
Gerhard Baer <Gerhard.Baer@protonmail.com>
2023-10-31 10:12:42 +0100

    fft_bench: transfer PyTorch tensor to numpy array.
    Signed-off-by: Gerhard Baer <Gerhard.Baer@protonmail.com>

--- fft_bench.py
+++ fft_bench.py
@@ -20,17 +20,17 @@ class TorchFft(object):
     Bridge to PyTorch FFT module.
     '''
     def fft(self, a, n=None, dim=-1, norm=None):
-        return torch.fft.fft(self.upload(a), n, dim, norm)
+        return self.download(torch.fft.fft(self.upload(a), n, dim, norm))
     def rfft(self, a, n=None, dim=-1, norm=None):
-        return torch.fft.rfft(self.upload(a), n, dim, norm)
+        return self.download(torch.fft.rfft(self.upload(a), n, dim, norm))
     def fft2(self, a, s=None, dim=(-2, -1), norm=None):
-        return torch.fft.fft2(self.upload(a), s, dim, norm)
+        return self.download(torch.fft.fft2(self.upload(a), s, dim, norm))
     def rfft2(self, a, s=None, dim=(-2, -1), norm=None):
-        return torch.fft.rfft2(self.upload(a), s, dim, norm)
+        return self.download(torch.fft.rfft2(self.upload(a), s, dim, norm))
     def fftn(self, a, s=None, dim=None, norm=None):
-        return torch.fft.fftn(self.upload(a), s, dim, norm)
+        return self.download(torch.fft.fftn(self.upload(a), s, dim, norm))
     def rfftn(self, a, s=None, dimp=None, norm=None):
-        return torch.fft.rfftn(self.upload(a), s, dim, norm)
+        return self.download(torch.fft.rfftn(self.upload(a), s, dim, norm))
 
 
 class TorchFft_CPU(TorchFft):
@@ -39,6 +39,8 @@ class TorchFft_CPU(TorchFft):
     '''
     def upload(self, a):
         return torch.from_numpy(a)
+    def download(self, a):
+        return a.numpy()
 
 
 # Mark which FFT submodules are available...
